f7aa7edcb4dc9196accbc737b70ee136eb189d28
Ola Sandstrom <s.o.sandstrom@gmail.com>
2010-05-30 23:42:56 +0000

    TestController, persist(fields, manyToOnes)...

---
+++ basic-plugin-test/src/test/java/net/sf/mardao/test/TestController.java
@@ -0,0 +1,22 @@
+package net.sf.mardao.test;
+
+import net.sf.mardao.test.basic.dao.EmployeeDao;
+import net.sf.mardao.test.basic.dao.OrganizationDao;
+import net.sf.mardao.test.basic.domain.Employee;
+import net.sf.mardao.test.basic.domain.Organization;
+
+public class TestController {
+	private final EmployeeDao employeeDao;
+	private final OrganizationDao organizationDao;
+	
+	public TestController(EmployeeDao employeeDao, OrganizationDao organizationDao) {
+		this.employeeDao = employeeDao;
+		this.organizationDao = organizationDao;
+	}
+	
+	public void populate() {
+		Organization wadpam = organizationDao.persist("Wadpam AB");
+		Employee ola = employeeDao.persist("Ola", wadpam);
+		
+	}
+}

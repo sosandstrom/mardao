aea878ef77adbe8fa424a5b915c30cbbd68ffc15
Ola Sandstrom <s.o.sandstrom@gmail.com>
2010-06-05 23:49:57 +0000

    Use COLUMN_NAME_${field.name.toUpperCase()} everywhere

---
+++ basic-webapp/src/main/resources/MySQL/CREATE-Organization.sql
@@ -0,0 +1,8 @@
+-- Created by mardao DAO generator 2010-06-06T06:47:53.042+0700
+-- CREATE script for TABLE Organization corresponding to Entity Organization 
+--
+CREATE TABLE `Organization` (
+	`id` INTEGER NOT NULL,
+	`name` VARCHAR(255) DEFAULT NULL, 
+	PRIMARY KEY (`id`)	
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

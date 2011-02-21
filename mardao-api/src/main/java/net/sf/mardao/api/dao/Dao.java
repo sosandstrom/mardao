c18af97881e6da3fc88255a5954c30daf6206a34
Ola Sandstrom <s.o.sandstrom@gmail.com>
2011-02-21 15:20:17 +0000

    findByPrimaryKey(Object parentKey, ID primaryKey)

--- mardao-api/src/main/java/net/sf/mardao/api/dao/Dao.java
+++ mardao-api/src/main/java/net/sf/mardao/api/dao/Dao.java
@@ -4,17 +4,19 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface Dao<T, ID extends Serializable> {
-	void delete(T entity);
-	
-	void delete(List<ID> primaryKeys);
-	
-	int deleteAll();
+    void delete(T entity);
 
-	List<T> findAll();
-	
-	T findByPrimaryKey(ID primaryKey);
+    void delete(List<ID> primaryKeys);
 
-	void persist(T entity);
+    int deleteAll();
 
-	void update(T entity);
+    List<T> findAll();
+
+    T findByPrimaryKey(ID primaryKey);
+
+    T findByPrimaryKey(Object parentKey, ID primaryKey);
+
+    void persist(T entity);
+
+    void update(T entity);
 }

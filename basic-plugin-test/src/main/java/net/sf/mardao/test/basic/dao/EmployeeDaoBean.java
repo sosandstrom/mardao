c9b0b632e2fa99369d17ef3eb5d8ffd45bf6e558
Vathanak Mao <vm@goldengekko.com>
2012-01-23 08:57:53 +0000

    Added Expression.OR and Expression.AND

--- basic-plugin-test/src/main/java/net/sf/mardao/test/basic/dao/EmployeeDaoBean.java
+++ basic-plugin-test/src/main/java/net/sf/mardao/test/basic/dao/EmployeeDaoBean.java
@@ -11,32 +11,29 @@ import net.sf.mardao.test.basic.domain.OrganizationUnit;
 /**
  * Implementation of Business Methods related to entity Employee.
  */
-public class EmployeeDaoBean 
-	extends AbstractEmployeeDao
-		implements EmployeeDao 
-{
-	private final OrganizationDao organizationDao;
-	
-	public EmployeeDaoBean(OrganizationDao organizationDao) {
-		this.organizationDao = organizationDao;
-	}
-	
-	// Implement your business methods here
-	public Employee findBy(String name, String signum, OrganizationUnit unit, Organization org) {
-		HashMap<String,Object> args = new HashMap<String,Object>();
-		
-		args.put(COLUMN_NAME_CURRENTEMPLOYER, org);
-		args.put(COLUMN_NAME_CURRENTUNIT, unit);
-		args.put(COLUMN_NAME_NAME, name);
-		args.put(COLUMN_NAME_SIGNUM, signum);
-		
-		return genericDao.findBy(args);
-	}
+public class EmployeeDaoBean
+        extends AbstractEmployeeDao
+        implements EmployeeDao {
+    private final OrganizationDao organizationDao;
 
-	public List<Employee> findByOrganization(String orgName) {
-		Expression org = new Expression(organizationDao.COLUMN_NAME_NAME, "=", orgName);
-		Expression.Foreign<Organization, Long> foreign = new Expression.Foreign<Organization, Long>(
-				COLUMN_NAME_CURRENTEMPLOYER, "=", (GenericDaoImpl<Organization, Long>) organizationDao, org);
-		return findBy(COLUMN_NAME_NAME, true, -1, 0, foreign);
-	}
+    public EmployeeDaoBean(OrganizationDao organizationDao) {
+        this.organizationDao = organizationDao;
+    }
+
+    // Implement your business methods here
+    public Employee findBy(String name, String signum, OrganizationUnit unit, Organization org) {
+        HashMap<String, Object> args = new HashMap<String, Object>();
+
+        args.put(COLUMN_NAME_CURRENTEMPLOYER, org);
+        args.put(COLUMN_NAME_CURRENTUNIT, unit);
+        args.put(COLUMN_NAME_NAME, name);
+        args.put(COLUMN_NAME_SIGNUM, signum);
+
+        return genericDao.findBy(args);
+    }
+
+    public List<Employee> findByOrganization(String orgName) {
+        Organization org = organizationDao.findByName(orgName);
+        return findByCurrentEmployer(org);
+    }
 }

d49a4a3d9a40293b8b75fcc3d7e4de5066b9fdac
Ola Sandstrom <s.o.sandstrom@gmail.com>
2014-12-22 19:24:08 +0100

    CursorPage serializable, Dao.getKind()

--- mardao-core/src/main/java/net/sf/mardao/dao/AbstractDao.java
+++ mardao-core/src/main/java/net/sf/mardao/dao/AbstractDao.java
@@ -260,6 +260,10 @@ public class AbstractDao<T, ID extends Serializable> {
     return mapper.toKey(parentKey, id);
   }
 
+  public String getKind() {
+    return mapper.getKind();
+  }
+
   public void setParentKey(T entity, Object parentKey) {
     mapper.setParentKey(entity, parentKey);
   }

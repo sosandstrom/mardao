9204ad9ce5cb316532ad8f56ca14ef673979fb63
Mattias Levin <mattias.levin@gmail.com>
2015-01-20 13:41:38 +0100

    Adde unit tests for cache annotations (partly)

--- mardao-maven-plugin/src/test/java/net/sf/mardao/test/dao/DCachedEntityDaoTest.java
+++ mardao-maven-plugin/src/test/java/net/sf/mardao/test/dao/DCachedEntityDaoTest.java
@@ -1,21 +1,27 @@
 package net.sf.mardao.test.dao;
 
+import com.google.common.collect.ImmutableList;
+import edu.emory.mathcs.backport.java.util.Arrays;
 import net.sf.mardao.dao.InMemorySupplier;
 import net.sf.mardao.dao.Supplier;
+import net.sf.mardao.test.domain.DCachedEntity;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.cache.annotation.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class DCachedEntityDaoTest {
 
-  private DCachedEntityDaoBean dao;
-
   @Before
   public void setUp() throws Exception {
-    Supplier supplier = new InMemorySupplier();
-    dao = new DCachedEntityDaoBean(supplier);
   }
 
   @After
@@ -24,12 +30,43 @@ public class DCachedEntityDaoTest {
 
   @Test
   public void testCrudAnnotations() throws Exception {
-    // TODO
+
+    // Check that jsr107 annotations were added to crud methods
+
+    Method readMethod = DCachedEntityDaoBean.class.getMethod("get", Object.class, Long.class);
+    assertTrue(readMethod.isAnnotationPresent(CacheResult.class));
+    Annotation[][] paramsAnnotations = readMethod.getParameterAnnotations();
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[0]), CacheKey.class));
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[1]), CacheKey.class));
+
+    Method putMethod = DCachedEntityDaoBean.class.getMethod("put", Object.class, Long.class, DCachedEntity.class);
+    assertTrue(putMethod.isAnnotationPresent(CachePut.class));
+    paramsAnnotations = putMethod.getParameterAnnotations();
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[0]), CacheKey.class));
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[1]), CacheKey.class));
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[2]), CacheValue.class));
+
+    Method removeMethod = DCachedEntityDaoBean.class.getMethod("delete", Object.class, Long.class);
+    assertTrue(removeMethod.isAnnotationPresent(CacheRemove.class));
+    paramsAnnotations = removeMethod.getParameterAnnotations();
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[0]), CacheKey.class));
+    assertTrue(hasAnnotation(Arrays.asList(paramsAnnotations[1]), CacheKey.class));
+
+  }
+
+
+  private static boolean hasAnnotation(Collection<Annotation> annotations, Class<? extends Annotation> annotationClass) {
+    for (Annotation annotation : annotations) {
+      if (annotation.annotationType().equals(annotationClass)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @Test
   public void testClassAnnotation() throws Exception {
-    // TODO
+    assertTrue(DCachedEntityDaoBean.class.isAnnotationPresent(CacheDefaults.class));
   }
 
   @Test
